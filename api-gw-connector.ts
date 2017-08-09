50ec1949024d2805f76d74326f2c99d93d64cae7
alexandre-walzberg <alexandre.walzberg@merapar.com>
2017-08-09 15:02:11 +0200

    first commit

---
+++ api-gw-connector.ts
@@ -0,0 +1,70 @@
+import { BaseNetworkInterface, RequestAndOptions, ResponseAndOptions, printRequest } from "apollo-client/transport/networkInterface";
+import { MiddlewareInterface } from "apollo-client/transport/middleware";
+import { AfterwareInterface } from "apollo-client/transport/afterware";
+import { ExecutionResult } from "graphql";
+import { Request as RequestMyApollo, HTTPNetworkInterface } from "apollo-client/transport/networkInterface";
+
+
+export interface AwsApiGwClient {
+  isAuthenticated(): Boolean;
+  authenticationExpired(): void;
+  graphqlPost(param: Object, body: Object, extra: Object): Promise<any>;
+}
+
+export class AwsApiGwNetworkInterface extends BaseNetworkInterface {
+  public _middlewares: MiddlewareInterface[];
+  public _afterwares: AfterwareInterface[];
+  private awsApiGatewayClient: AwsApiGwClient;
+
+  constructor(client: AwsApiGwClient) {
+    super('http://');
+    this.awsApiGatewayClient = client;
+  }
+
+  public fetchFromAwsApiGw({
+    request,
+    options,
+  }: RequestAndOptions): Promise<Response> {
+    return this.awsApiGatewayClient.graphqlPost(
+      {},
+      printRequest(request),
+      { request, options }
+    );
+  };
+
+  public query(request: RequestMyApollo): Promise<ExecutionResult> {
+    const options = { ...this._opts };
+
+    if(!this.awsApiGatewayClient.isAuthenticated()) {
+      return new Promise(function(resolve: any, reject: any){
+        reject(new Error('AWS API GW client not ready/authenticated !'));
+      });
+    } else {
+      return this.fetchFromAwsApiGw({request, options})
+        .then( ( response: any ) => {
+          const httpResponse = response['data'] as Response;
+
+          if (response.status === 401 || response.status === 403) {
+            this.awsApiGatewayClient.authenticationExpired();
+          }
+
+          if (response.status >= 300) {
+            const httpError = new Error(`Network request failed with status ${response.status} - "${response.statusText}"`);
+            (httpError as any).response = httpResponse;
+
+            throw httpError;
+          }
+          return httpResponse;
+        })
+        .then((payload) => {
+          if (!payload.hasOwnProperty('data') && !payload.hasOwnProperty('errors')) {
+            throw new Error(
+              `Server response was missing for query '${request.debugName}'.`,
+            );
+          } else {
+            return payload as ExecutionResult;
+          }
+        });
+    }
+  };
+}
